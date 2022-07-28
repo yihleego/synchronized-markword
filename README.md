9d6579bda5b5843499bd600148e9ee6c92e38027
leego <leegodev@hotmail.com>
2022-07-29 00:53:17 +0800

    tmp

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # Synchronized & Mark Word
 
 本文旨在验证不同场景下通过`synchronized`关键字修饰的对象的对象头的 Mark Word 的情况。
+
+## 覆盖 Java 版本
+
+- OpenJDK 7
+- OpenJDK 8
+- OpenJDK 9
+- OpenJDK 10
+- OpenJDK 11
+- OpenJDK 12
+- OpenJDK 13
+- OpenJDK 14
+- OpenJDK 15
+- OpenJDK 16
+- OpenJDK 17
+- OpenJDK 18
+
+## 结论
+
+### 偏向锁 Biased Locking
+
+- 偏向锁可重入。
+- 偏向锁会在竞争时候重置为无锁状态，这个过程称为偏向锁撤销，需要进入安全点（即STW）。
+- 偏向锁撤销时，如果前一个线程是存活状态，则会升级为轻量级锁，如果前一个线程是非存活状态，则重偏向。
+
+### 轻量级锁 Lightweight Locking
+
+- 轻量级锁可重入。
+- 两个线程竞争时
---
+++ src/test/java/io/leego/test/SynchronizedMarkWordTests.java
@@ -0,0 +1,295 @@
+package io.leego.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openjdk.jol.info.ClassLayout;
+
+import java.lang.management.ManagementFactory;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Leego Yih
+ */
+public class SynchronizedMarkWordTests {
+    private static final Object lock = new Object();
+
+    @BeforeEach
+    public void before() {
+        StringBuilder s = new StringBuilder();
+        s.append("Java Version: ");
+        s.append(System.getProperty("java.version")).append("\n");
+        s.append("Java VM: ");
+        s.append(System.getProperty("java.vm.name")).append("\n");
+        s.append("VM Options: ");
+        List<String> args = ManagementFactory.getRuntimeMXBean().getInputArguments();
+        for (String arg : args) {
+            if (arg.contains("-X")) {
+                s.append(arg).append(" ");
+            }
+        }
+        s.append("\n");
+        System.out.println(s);
+        //logger.debug("{}", s);
+    }
+
+    /**
+     * 偏向锁-重入
+     */
+    @Test
+    public void testBiasedLocking_Reenter() {
+        print("无锁时");
+
+        synchronized (lock) {
+            print("[当前线程] 获取锁");
+            synchronized (lock) {
+                print("[当前线程] 重入获取锁");
+            }
+            print("[当前线程] 重入释放锁");
+        }
+        print("[当前线程] 释放锁");
+    }
+
+    /**
+     * 偏向锁升级轻量级锁
+     * 当前线程进行加锁解锁操作，由于当前线程会一直处于存活状态，所以后一个线程无法获取偏向锁，升级为轻量级锁。
+     */
+    @Test
+    public void testBiasedLocking_Upgrade() {
+        print("无锁时");
+
+        synchronized (lock) {
+            print("[当前线程] 获取锁");
+        }
+        print("[当前线程] 释放锁");
+
+        sleep(1000);
+
+        Thread t0 = run(() -> {
+            synchronized (lock) {
+                print("[新线程] 获取锁");
+            }
+            print("[新线程] 释放锁");
+        });
+
+        waitUntilNotAlive(t0);
+    }
+
+    /**
+     * 偏向锁重偏向
+     * 三个线程依次获取锁，前一个线程执行完成，且线程为非存活状态后，再执行后一个线程。
+     */
+    @Test
+    public void testBiasedLocking_Rebias() {
+        print("无锁时");
+
+        Thread t0 = run(() -> {
+            synchronized (lock) {
+                print("[线程-0] 获取锁");
+            }
+            print("[线程-0] 释放锁");
+        });
+
+        waitUntilNotAlive(t0);
+        busy();
+        sleep(1000);
+        //System.gc();
+
+        Thread t1 = run(() -> {
+            synchronized (lock) {
+                print("[线程-1] 获取锁");
+            }
+            print("[线程-1] 释放锁");
+        });
+
+        waitUntilNotAlive(t1);
+        busy();
+        sleep(1000);
+        //System.gc();
+
+        Thread t2 = run(() -> {
+            synchronized (lock) {
+                print("[线程-2] 获取锁");
+            }
+            print("[线程-2] 释放锁");
+        });
+
+        waitUntilNotAlive(t2);
+    }
+
+    /**
+     * 轻量级锁-重入
+     * 禁用偏向锁 -XX:-UseBiasedLocking
+     */
+    @Test
+    public void testLightweightLocking_Reenter() {
+        print("无锁时");
+
+        synchronized (lock) {
+            print("[当前线程] 获取锁");
+            synchronized (lock) {
+                print("[当前线程] 重入获取锁");
+            }
+            print("[当前线程] 重入释放锁");
+        }
+        print("[当前线程] 释放锁");
+    }
+
+    /**
+     * 轻量级锁膨胀为重量级锁-自旋达到上限
+     * 后一个线程自旋达到上限，膨胀为重量级锁。
+     * 禁用偏向锁 -XX:-UseBiasedLocking
+     */
+    @Test
+    public void testLightweightLocking_InflateWithSpinLimit() {
+        print("无锁时");
+
+        Thread t0 = run(() -> {
+            synchronized (lock) {
+                print("[线程-0] 获取锁");
+                // 使第二个线程自旋达到上限
+                busy();
+            }
+            print("[线程-0] 释放锁");
+        });
+
+        Thread t1 = run(() -> {
+            synchronized (lock) {
+                print("[线程-1] 获取锁");
+            }
+            print("[线程-1] 释放锁");
+        });
+
+        Thread t2 = run(() -> {
+            synchronized (lock) {
+                print("[线程-2] 获取锁");
+            }
+            print("[线程-2] 释放锁");
+        });
+
+        waitUntilNotAlive(t0, t1, t2);
+    }
+
+    /**
+     * 轻量级锁膨胀为重量级锁-多线程竞争
+     * 三个线程同时竞争，直接膨胀为重量级锁。
+     * 禁用偏向锁 -XX:-UseBiasedLocking
+     */
+    @Test
+    public void testLightweightLocking_InflateWithMultithreading() {
+        print("无锁时");
+
+        Thread t0 = run(() -> {
+            synchronized (lock) {
+                print("[线程-0] 获取锁");
+            }
+            print("[线程-0] 释放锁");
+        });
+
+        Thread t1 = run(() -> {
+            synchronized (lock) {
+                print("[线程-1] 获取锁");
+            }
+            print("[线程-1] 释放锁");
+        });
+
+        Thread t2 = run(() -> {
+            synchronized (lock) {
+                print("[线程-2] 获取锁");
+            }
+            print("[线程-2] 释放锁");
+        });
+
+        waitUntilNotAlive(t0, t1, t2);
+    }
+
+    /**
+     * 重量级锁降级
+     * 禁用偏向锁 -XX:-UseBiasedLocking
+     */
+    @Test
+    public void testLightweightLocking_Deflate() {
+        print("无锁时");
+
+        Thread[] ts = new Thread[10];
+        for (int i = 0; i < ts.length; i++) {
+            final int f = i;
+            ts[i] = run(() -> {
+                synchronized (lock) {
+                    print("[线程-" + f + "] 获取锁");
+                }
+                print("[线程-" + f + "] 释放锁");
+            });
+        }
+
+        waitUntilNotAlive(ts);
+
+        print("无竞争后");
+
+        sleep(1000);
+
+        print("休眠一段时间后");
+
+        synchronized (lock) {
+            print("[当前线程] 再次获取锁");
+        }
+        print("[当前线程] 再次释放锁");
+    }
+
+    public Thread run(Runnable r) {
+        Thread t = new Thread(r);
+        t.start();
+        return t;
+    }
+
+    public void runUntilNotAlive(Runnable r) {
+        Thread t = new Thread(r);
+        t.start();
+        waitUntilNotAlive(t);
+        t = null;
+    }
+
+    public void waitUntilNotAlive(Thread t) {
+        while (true) {
+            if (!t.isAlive()) {
+                break;
+            }
+        }
+        t = null;
+    }
+
+    public void waitUntilNotAlive(Thread... ts) {
+        while (true) {
+            int count = 0;
+            for (Thread t : ts) {
+                if (!t.isAlive()) {
+                    count++;
+                }
+            }
+            if (count == ts.length) {
+                break;
+            }
+        }
+        ts = null;
+    }
+
+    public void print(String prefix) {
+        System.out.println(prefix + "\n" + ClassLayout.parseInstance(lock).toPrintable());
+        //logger.debug("{}\n{}", prefix, ClassLayout.parseInstance(lock).toPrintable());
+    }
+
+    public int busy() {
+        int adder = 0;
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            adder += i;
+        }
+        return adder;
+    }
+
+    public void sleep(long millis) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(millis);
+        } catch (InterruptedException ignored) {
+        }
+    }
+}
